e11e530c1a0aac9c72f8836991f2013c1da7a92c
Bhanu Saketh <118091571+BhanuSaketh@users.noreply.github.com>
2025-02-24 19:44:06 +0530

    Create structure.py

---
+++ Algorithms/Backtracking/structure.py
@@ -0,0 +1,8 @@
+def valid_function():
+  ##Code to check a sub answer is valid or not
+
+def backtracking_function():
+  ##Code for Genarating subsets
+  ##Resucrsively calling backtracking_function
+  valid_function()
+  ##Valid_function is called at each stage to check the sub answer is valid or not
