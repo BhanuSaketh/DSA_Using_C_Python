79392a80c7c25f53b7cabeeb7db0c59a69b4d2ee
Bhanu Saketh <118091571+BhanuSaketh@users.noreply.github.com>
2025-02-24 20:07:43 +0530

    Update subset.py

--- Algorithms/Backtracking/subset.py
+++ Algorithms/Backtracking/subset.py
@@ -1,13 +1,16 @@
 ##Given an integer array nums of unique elements, return all possible subsets
 temp,ans=[],[]
-def backtrack(nums,start,end):
+nums=[1,2,3]
+
+def backtrack(start,end):
   ans.append(temp[:])
   for i in range(start,end):
     temp.append(nums[i])
-    backtrack(nums,start+1,end)
+    backtrack(i+1,end)
     temp.pop()
 
-nums=[1,2,3]
+
 backtrack(nums,0,len(nums))
+print(ans)
 
 #output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
